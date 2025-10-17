4f19f4496c908f01dc8abcb2cb5b81da210433a5
Emilly Andrade <emilly.andrade.digital@gmail.com>
2025-10-17 16:04:24 -0300

    update

---
+++ js/app.js
@@ -0,0 +1,10 @@
+//* Adicionar Itens: Inclusão de novos produtos no carrinho.
+function adicionar(){
+
+}
+
+
+//* Calcular Subtotal: Atualização dinâmica do valor total dos itens.
+//* Calcular Valor Final: Cálculo do valor total da compra (Subtotal + Frete/Taxas, se aplicável).
+//* Limpar Carrinho: Função para remover todos os itens e zerar os valores.
+//* Remover Itens: Exclusão de produtos específicos do carrinho.
