d63e4d3756d93b0b75673215a7aaccb5cb0ae31e
Emilly Andrade <emilly.andrade.digital@gmail.com>
2025-10-20 16:11:55 -0300

    update

--- js/app.js
+++ js/app.js
@@ -1,41 +1,44 @@
 //* Limpar página para iniciar
 let totalGeral = 0;
-document.getElementById('lista-produtos') = '';
-document.getElementById('valor-total') = '';
-
-//* Adicionar Itens: Inclusão de novos produtos no carrinho. (recuperar nome, quantidade, e preço)
+document.getElementById('lista-produtos').innerHTML = '';
+document.getElementById('valor-total').innerHTML = 'R$';
+ 
 function adicionar(){
+    // (recuperar nome, quantidade, e preço)
     let produto = document.getElementById('produto').value;
     let quantidade = document.getElementById('quantidade').value;
+
+    if (quantidade <= 0){
+        alert('Por favor, insira uma quantidade válida.');
+        return;
+    }
+
     let nomeProduto = produto.split('-')[0];
-    let valorUnitario = produto.split('$')[1];
+    let valorUnitario = Number(produto.split('$')[1]);
 
     //* Calcular Subtotal: Atualização dinâmica do valor total dos itens.
     let preco = quantidade * valorUnitario;
 
     //* Adicionar produto no carrinho
     let carrinho = document.getElementById('lista-produtos');   
-    carrinho.innerHTML = carrinho.innerHTML + `<span class="texto-laranja">${quantidade}</span> ${nomeProduto} <span class="texto-laranja">R$${preco}</span>` 
+    carrinho.innerHTML += `<section class="carrinho__produtos__produto">
+          <span class="texto-rosa">${quantidade}x</span> ${nomeProduto} <span class="texto-rosa">R$${preco}</span>
+        </section>`
 
     //* Calcular Valor Final: Cálculo do valor total da compra (Subtotal).
-    totalGeral = totalGeral + preco;
+    totalGeral += preco;
     let campoTotal = document.getElementById('valor-total');
     campoTotal.textContent = `R$${totalGeral}`;
 
-
-    alert('Produto: ' + produto);
-    alert('Quantidade: ' + quantidade);
-    alert('Nome: ' +nomeProduto);
-    alert('Preço unitario: R$' + precoUnidade);
-    alert('Preço final: R$' +precoTotalProduto);
+    document.getElementById('quantidade').value = '';
 }
-
-
-
-
-//* Remover Itens: Exclusão de produtos específicos do carrinho.
 
 //* Limpar Carrinho: Função para remover todos os itens e zerar os valores.
 function limpar(){
+    totalGeral = 0;
 
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$0'
 }
+
+limpar();
